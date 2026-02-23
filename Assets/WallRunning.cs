b038ee5e54a1f288ea80725820e263d835566be3
Mathvib77 <m.vibertcapou@bdx.ecolescreatives.com>
2026-02-23 17:56:29 +0100

    Started working on wallrunning and added assets

---
+++ Assets/WallRunning.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float maxWallRunTime;
+    public float wallRunTimer;
+
+    [Header("Input")]
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallhit;
+    private RaycastHit rightWallhit;
+    private bool wallLeft;
+    private bool wallRight;
+
+    [Header("References")]
+    public Transform orientation;
+    private PlayerMovement pm;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        CheckForWall();
+    }
+
+    //Wall Check
+    private void CheckForWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallhit, wallCheckDistance, whatIsWall);
+    }
+
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
+
+    private void StateMachine()
+    {
+        //Getting Inputs
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //State 1 wallrunning
+        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        {
+            //start wallrun here !
+        }
+    }
+
+    private void StartWallRun()
+    {
+
+    }
+
+    private void WallRunningMovement()
+    {
+        
+    }
+
+    private void StopWallRun()
+    {
+
+    }
+}
